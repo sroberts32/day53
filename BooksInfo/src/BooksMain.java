a10782c509f56e2ad95aadf5e32ac1a84f284b7a
unknown <crimm_32@hotmail.com>
2022-05-11 17:44:01 -0400

    fixed scanner

--- BooksInfo/src/BooksMain.java
+++ BooksInfo/src/BooksMain.java
@@ -13,27 +13,26 @@ public class BooksMain {
         books.add(new Book("Dark Force Rising", 480, 1992));
         books.add(new Book("Blood of Elves", 320, 1994));
 
-
         while (true) {
             System.out.println("What would you like to print?");
             System.out.println("Options: Everything, title, exit");
             String response = scanner.nextLine();
 
-        if (response.equalsIgnoreCase("exit")) {
-            break;
-        } else if (response.equalsIgnoreCase("everything")) {
-            for (Book book : books) {
-                System.out.println(book);
+            if (response.equalsIgnoreCase("exit")) {
+                break;
+            } else if (response.equalsIgnoreCase("everything")) {
+                for (Book book : books) {
+                    System.out.println(book);
+                }
+            } else if (response.equalsIgnoreCase("title")) {
+                for (int i = 0; i < books.size(); i++) {
+                    System.out.println(books.get(i).getTitle());
+                }
+
             }
-        } else if (response.equalsIgnoreCase("title")) {
-            for (int i = 0; i < books.size(); i++) {
-                System.out.println(books.get(i).getTitle());
-            }
-        
+
         }
-
-    }  
+        scanner.close();
     }
 
-    }
-
+}
